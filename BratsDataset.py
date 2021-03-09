1cad39b6a4c354d15a2659bd7f9db05870160b17
Yukei7 <yukei0509@gmail.com>
2021-03-09 17:38:19 +0800

    Modified: Dataset class Implement train-test split with sklearn.

--- BratsDataset.py
+++ BratsDataset.py
@@ -2,28 +2,41 @@ import torch.utils.data as data
 import h5py
 import os
 import torch
+import numpy as np
 
 
 class BratsDataset(data.Dataset):
-    def __init__(self, h5folder, list_ids):
+    def __init__(self, h5folder, transform=None):
         self.h5list = os.listdir(h5folder)
-        self.list_ids = list_ids
-        self.h5files = [os.path.join(h5folder, self.h5list[idx]) for idx in list_ids]
+        self.h5files = [os.path.join(h5folder, x) for x in self.h5list]
+        self.transform = transform
 
     def __len__(self):
-        return len(self.list_ids)
+        return len(self.h5list)
 
     def __getitem__(self, item):
-        index = self.list_ids[item]
-        h5file = self.h5files[index]
-        f = h5py.File(h5file)
+        h5file = self.h5files[item]
+        f = h5py.File(h5file, 'r')
         X, y = f['X'][:], f['y'][:]
         f.close()
-        return torch.from_numpy(X), torch.from_numpy(y)
+        X, y = self.preprocess(X, y)
+        return torch.from_numpy(X / 1.0), torch.from_numpy(y / 1.0)
+
+    def preprocess(self, data, label):
+        c, h, w = data.shape
+        h_gt, w_gt = label.shape
+        # make sure that the data and label have same size
+        assert h == h_gt, "Error"
+        assert w == w_gt, "Error"
+
+        if self.transform:
+            data, label = self.transform(data, label)
+        label = label[np.newaxis, :, :]
+        return data, label
 
 
 if __name__ == "__main__":
-    dataset = BratsDataset('brats19_h5', [1, 2, 3, 4])
+    dataset = BratsDataset('brats19_h5')
     print(len(dataset))
     X_sample, y_sample = dataset[0]
     print(X_sample.shape, y_sample.shape)
--- Train.py
+++ Train.py
@@ -3,14 +3,18 @@ import pandas as pd
 import numpy as np
 import os
 from BratsDataset import BratsDataset
+from sklearn.model_selection import train_test_split, KFold
 from torch.utils import data
-from utils import split_dataset
 import torchvision
 import torch
+from torchvision.models.segmentation import fcn, deeplabv3
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--input", type=str, default="brats19_h5")
 parser.add_argument("--seed", type=int, default=2021)
+parser.add_argument("--testratio", type=float, default=0.8)
+parser.add_argument("--nsplits", type=int, default=5)
+parser.add_argument("--epoch", type=int, default=50)
 args = parser.parse_args()
 
 
@@ -19,19 +23,46 @@ def init():
     torch.manual_seed(args.seed)
 
 
+def train():
+    pass
+
+
+def validation():
+    pass
+
+
 def main():
-    train_id, test_id = split_dataset(0.8, len(os.listdir(args.input)))
-    # TODO: Cross-val
-    t_id, val_id = split_dataset(0.8, len(train_id))
-    datasets = {x: BratsDataset(args.input, y)
-                for (x, y) in [('train', t_id), ('val', val_id)]}
-    dataloaders = {x: data.DataLoader(datasets[x], batch_size=4, shuffle=True, num_workers=4)
-                   for x in ['train', 'val']}
+    # split train set and test set
+    dataset = BratsDataset(args.input)
+    train_idx, test_idx = train_test_split(list(range(len(dataset))), test_size=args.testratio)
+    datasets = {x: data.Subset(dataset, y)
+                for (x, y) in [("train", train_idx), ("test", test_idx)]}
+    del dataset
+
+    # k-fold cross-val
+    kf = KFold(args.nsplits)
+    fold_counter = 1
+    for train_idx, val_idx in kf.split(datasets["train"]):
+        print(f"Fold [{fold_counter}/{args.nsplits}]:")
+        # k-fold
+        trainset, valset = data.Subset(datasets["train"], train_idx), data.Subset(datasets["train"], val_idx)
+        train_dataloader = data.DataLoader(trainset, 8, shuffle=True, num_workers=4)
+
+        # go through each fold
+        for epoch in range(1, args.epoch):
+            print(f"Epoch [{epoch}/{args.epoch}]:")
+            # Train
+
+            # Val
+
+            # Save model
+
+        fold_counter += 1
 
     use_cuda = torch.cuda.is_available()
     device = torch.device("cuda:0" if use_cuda else "cpu")
 
 
 if __name__ == "__main__":
-    np.random.seed(args.seed)
-    split_dataset()
+    init()
+    main()
